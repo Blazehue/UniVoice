034f7e1b2282222c039c9ced6f6520e3d7c0567f
Rajat <gamashockx@gmail.com>
2025-01-30 04:06:33 +0530

    Update PStudent.tsx

--- src/components/PStudent.tsx
+++ src/components/PStudent.tsx
@@ -88,7 +88,7 @@ const PStudent: React.FC = () => {
                 />
               </div>
               <button className="bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition-colors">
-                Schedule Meeting
+                Lock the slot
               </button>
             </div>
           </div>
